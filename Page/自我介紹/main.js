3170db99ecc65db0d5bb4b92d569fa6263635817
DevonEvant <horningch@gmail.com>
2021-09-01 17:28:44 +0800

    自介

--- Page/自我介紹/main.js
+++ Page/自我介紹/main.js
@@ -1,5 +1,3 @@
-import * as controlAPI from "./control";
-import "./main.css";
 const ConversationBox = new controlAPI.ConversationBoxController();
 // const c: any = new ConversationIteratorGenerator(conversationRecode().init)
 // console.log(c.next().value);
