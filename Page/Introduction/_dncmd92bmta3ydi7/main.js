59d7cff11d47568355fe80a96657cb605406cdd9
DevonEvant <horningch@gmail.com>
2022-04-24 09:28:20 +0800

    *

---
+++ Page/Introduction/_dncmd92bmta3ydi7/main.js
@@ -0,0 +1,11 @@
+"use strict";
+const ConversationBox = new ConversationBoxController();
+// const c: any = new ConversationIteratorGenerator(conversationRecode().init)
+// console.log(c.next().value);
+ConversationBox.nextConversation();
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+//# sourceMappingURL=main.js.map
---
+++ Page/Introduction/_dncmd92bmta3ydi7/main.ts
@@ -0,0 +1,15 @@
+const ConversationBox = new ConversationBoxController();
+// const c: any = new ConversationIteratorGenerator(conversationRecode().init)
+
+// console.log(c.next().value);
+
+
+ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+// ConversationBox.nextConversation()
+
+
+
